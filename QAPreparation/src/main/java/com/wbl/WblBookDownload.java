e2fd662b91ec758b1e6b09491100fee8d9496e83
xaviermary19 <55524296+xaviermary19@users.noreply.github.com>
2020-01-24 12:36:29 -0800

    Add files via upload

---
+++ QAPreparation/src/main/java/com/wbl/WblBookDownload.java
@@ -0,0 +1,60 @@
+package com.wbl;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class WblBookDownload {
+	
+	WebDriver driver;
+	
+	@BeforeClass
+	public void beforeClass()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\antoj\\eclipse-workspace\\QAPreparation\\QAPreparation\\resources\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void bookDownload() throws InterruptedException
+	{
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://www.whiteboxqa.com/");
+		
+		driver.findElement(By.xpath("//*[@id='loginButton']//parent::li")).click();
+		driver.findElement(By.cssSelector("#username")).sendKeys("xaviermary19@gmail.com");
+		driver.findElement(By.cssSelector("#password")).sendKeys("Mary@4012");
+		driver.findElement(By.xpath("//*[@id=\"login\"]")).click();
+		
+		Actions action =  new Actions(driver);
+		WebElement resource = driver.findElement(By.xpath("//*[@id=\"navbar-collapse\"]/ul/li[4]/a"));
+		action.moveToElement(resource).perform();
+		
+		WebElement presentations = driver.findElement(By.xpath("//*[@id=\"navbar-collapse\"]/ul/li[4]/ul/li[2]/a"));
+		presentations.click();
+		
+		driver.findElement(By.xpath("//a[@id='books']")).click();
+		
+		driver.findElement(By.xpath("//*[@id=\"books\"]/div/div/div/div/table/tbody/tr[9]/td[2]/a")).click();
+		
+		driver.navigate().to("https://drive.google.com/u/0/uc?id=0B2Z9J2D1hx7UM0xnRkF3aG5mZ0E&export=download");
+		Thread.sleep(5000);
+	}
+	
+	
+	@AfterClass
+	public void afterClass()
+	{
+		driver.quit();
+	}
+
+}
